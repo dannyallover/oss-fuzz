fde5f905f50e8f7102778e264943f9ba124eaebd
Danny Halawi <dhalawi@google.com>
2020-07-22 04:47:41 +0000

    simplifying functions by improving size calculation

--- projects/asn1-proto/asn1_proto_to_der.cc
+++ projects/asn1-proto/asn1_proto_to_der.cc
@@ -2,7 +2,8 @@
 
 namespace asn1_proto {
 
-uint8_t ASN1ProtoToDER::GetVariableIntLen(const uint64_t value, const size_t base) {
+uint8_t ASN1ProtoToDER::GetVariableIntLen(const uint64_t value,
+                                          const size_t base) {
   uint8_t base_bits = log2(base);
   for (uint8_t num_bits = (sizeof(value) - 1) * 8; num_bits >= base_bits;
        num_bits -= base_bits) {
@@ -16,77 +17,70 @@ uint8_t ASN1ProtoToDER::GetVariableIntLen(const uint64_t value, const size_t bas
 
 void ASN1ProtoToDER::InsertVariableInt(const size_t value, const size_t pos) {
   std::vector<uint8_t> len_vec;
-  for (uint8_t shift = GetNumBytes(value, 256); shift != 0; --shift) {
+  for (uint8_t shift = GetVariableIntLen(value, 256); shift != 0; --shift) {
     len_vec.push_back((value >> ((shift - 1) * 8)) & 0xFF);
   }
   encoder_.insert(encoder_.begin() + pos, len_vec.begin(), len_vec.end());
 }
 
-size_t ASN1ProtoToDER::EncodeOverrideLength(const std::string raw_len,
-                                            const size_t len_pos) {
+void ASN1ProtoToDER::EncodeOverrideLength(const std::string raw_len,
+                                          const size_t len_pos) {
   encoder_.insert(encoder_.begin() + len_pos, raw_len.begin(), raw_len.end());
-  return raw_len.size();
 }
 
-size_t ASN1ProtoToDER::EncodeIndefiniteLength(const size_t len_pos) {
-  AppendVariableInt(0x80, len_pos);
+void ASN1ProtoToDER::EncodeIndefiniteLength(const size_t len_pos) {
+  InsertVariableInt(0x80, len_pos);
   // The PDU's value is from |len_pos| to the end of |encoder_|, so just add an
   // EOC marker to the end.
-  AppendVariableInt(0x00, encoder_.size());
-  AppendVariableInt(0x00, encoder_.size());
-  return 3;
+  InsertVariableInt(0x00, encoder_.size());
+  InsertVariableInt(0x00, encoder_.size());
 }
 
-size_t ASN1ProtoToDER::EncodeDefiniteLength(const size_t actual_len,
-                                            const size_t len_pos) {
-  AppendVariableInt(actual_len, len_pos);
-  size_t len_num_bytes = GetNumBytes(actual_len, 256);
+void ASN1ProtoToDER::EncodeDefiniteLength(const size_t actual_len,
+                                          const size_t len_pos) {
+  InsertVariableInt(actual_len, len_pos);
+  size_t len_num_bytes = GetVariableIntLen(actual_len, 256);
   // X.690 (2015), 8.1.3.3: The long-form is used when the length is
   // larger than 127.
-  // Note: |len_num_bytes| is not checked here, because it will return
+  // Note: |len_num_bytes| is not checked here, because it will equal
   // 1 for values [128..255], but those require the long-form length.
   if (actual_len > 127) {
     // See X.690 (2015) 8.1.3.5.
     // Long-form length is encoded as a byte with the high-bit set to indicate
     // the long-form, while the remaining bits indicate how many bytes are used
     // to encode the length.
-    AppendVariableInt((0x80 | len_num_bytes), len_pos);
-    len_num_bytes += 1;
+    InsertVariableInt((0x80 | len_num_bytes), len_pos);
   }
-  return len_num_bytes;
 }
 
-size_t ASN1ProtoToDER::EncodeLength(const Length& len,
-                                    const size_t actual_len,
-                                    const size_t len_pos) {
+void ASN1ProtoToDER::EncodeLength(const Length& len,
+                                  const size_t actual_len,
+                                  const size_t len_pos) {
   if (len.has_length_override()) {
-    return EncodeOverrideLength(len.length_override(), len_pos);
+    EncodeOverrideLength(len.length_override(), len_pos);
   } else if (len.has_indefinite_form() && len.indefinite_form()) {
-    return EncodeIndefiniteLength(len_pos);
+    EncodeIndefiniteLength(len_pos);
   } else {
-    return EncodeDefiniteLength(actual_len, len_pos);
+    EncodeDefiniteLength(actual_len, len_pos);
   }
 }
 
-size_t ASN1ProtoToDER::EncodeValue(const Value& val) {
-  size_t len = 0;
+void ASN1ProtoToDER::EncodeValue(const Value& val) {
   for (const auto& val_ele : val.val_array()) {
     if (val_ele.has_pdu()) {
-      len += EncodePDU(val_ele.pdu());
+      EncodePDU(val_ele.pdu());
     } else {
-      len += val_ele.val_bits().size();
       encoder_.insert(encoder_.end(), val_ele.val_bits().begin(),
                       val_ele.val_bits().end());
     }
   }
-  return len;
 }
 
-uint8_t ASN1ProtoToDER::EncodeHighTagNumberForm(const uint8_t id_class,
-                                                const uint8_t encoding,
-                                                const uint32_t tag_num) {
+void ASN1ProtoToDER::EncodeHighTagNumberForm(const uint8_t id_class,
+                                             const uint8_t encoding,
+                                             const uint32_t tag_num) {
   // The high-tag-number form base 128 encodes |tag_num| (X.690 (2015), 8.1.2).
-  uint8_t num_bytes = GetNumBytes(tag_num, 128);
+  uint8_t num_bytes = GetVariableIntLen(tag_num, 128);
   // High-tag-number form requires the lower 5 bits of the identifier to be set
   // to 1 (X.690 (2015), 8.1.2.4.1).
   uint64_t id_parsed = (id_class | encoding | 0x1F);
@@ -98,11 +92,10 @@ uint8_t ASN1ProtoToDER::EncodeHighTagNumberForm(const uint8_t id_class,
     id_parsed <<= 8;
   }
   id_parsed |= (tag_num & 0x7F);
-  AppendVariableInt(id_parsed, encoder_.size());
-  return num_bytes + 1;
+  InsertVariableInt(id_parsed, encoder_.size());
 }
 
-uint8_t ASN1ProtoToDER::EncodeIdentifier(const Identifier& id) {
+void ASN1ProtoToDER::EncodeIdentifier(const Identifier& id) {
   // The class comprises the 7th and 8th bit of the identifier (X.690
   // (2015), 8.1.2).
   uint8_t id_class = static_cast<uint8_t>(id.id_class()) << 6;
@@ -115,10 +108,9 @@ uint8_t ASN1ProtoToDER::EncodeIdentifier(const Identifier& id) {
   // When the tag number is greater than or equal to 31, encode with a single
   // byte; otherwise, use the high-tag-number form (X.690 (2015), 8.1.2).
   if (tag_num >= 31) {
-    return EncodeHighTagNumberForm(id_class, encoding, tag_num);
+    EncodeHighTagNumberForm(id_class, encoding, tag_num);
   }
-  AppendVariableInt((id_class | encoding | tag_num), encoder_.size());
-  return 1;  // low-tag-number form requires 1 byte to encode.
+  InsertVariableInt((id_class | encoding | tag_num), encoder_.size());
 }
 
 size_t ASN1ProtoToDER::EncodePDU(const PDU& pdu) {
@@ -127,25 +119,18 @@ size_t ASN1ProtoToDER::EncodePDU(const PDU& pdu) {
   if (depth_ > 67000) {
     return 0;
   }
-  uint8_t id_len = EncodeIdentifier(pdu.id());
-  size_t len_pos = encoder_.size();
-  size_t val_len = EncodeValue(pdu.val());
-  size_t len_len = EncodeLength(pdu.len(), val_len, len_pos);
+  size_t size_before_insertion = encoder_.size();
+  EncodeIdentifier(pdu.id());
+  size_t id_size = encoder_.size() - size_before_insertion;
+  EncodeValue(pdu.val());
+  size_t value_size = encoder_.size() - size_before_insertion - id_size;
+  EncodeLength(pdu.len(), value_size, size_before_insertion + id_size);
   --depth_;
-  return id_len + val_len + len_len;
-}
-
-void ASN1ProtoToDER::PrintEncodedBits() {
-  for (const uint8_t byte : encoder_) {
-    for (int i = 7; i >= 0; --i) {
-      if (((byte >> i) & 0x01)) {
-        std::cout << "1";
-      } else {
-        std::cout << "0";
-      }
-    }
-    std::cout << " ";
-  }
+  // The size of the PDU is going to be the
+  // size of |encoder_| after the PDU was inserted
+  // subtracted by the size of |encoder_| before
+  // the PDU was inserted.
+  return encoder_.size() - size_before_insertion;
 }
 
 std::vector<uint8_t> ASN1ProtoToDER::ProtoToDER(const PDU& pdu) {
--- projects/asn1-proto/asn1_proto_to_der.h
+++ projects/asn1-proto/asn1_proto_to_der.h
@@ -25,53 +25,46 @@ class ASN1ProtoToDER {
 
   // Encodes |id| to DER according to X.690 (2015), 8.1.2.
   // Returns number of bytes used to encode |id|.
-  uint8_t EncodeIdentifier(const Identifier& id);
+  void EncodeIdentifier(const Identifier& id);
 
   // Concatinates |id_class|, |encoding|, and |tag| according to DER
-  // high-tag-number form rules (X.690 (2015), 8.1.2.4), returning
-  // number of bytes used to encode the identifier.
-  uint8_t EncodeHighTagNumberForm(const uint8_t id_class,
-                                  const uint8_t encoding,
-                                  const uint32_t tag);
+  // high-tag-number form rules (X.690 (2015), 8.1.2.4).
+  void EncodeHighTagNumberForm(const uint8_t id_class,
+                               const uint8_t encoding,
+                               const uint32_t tag);
 
-  // Encodes the length in |actual_len| to DER, returning the length
-  // in bytes of the encoded length. |len_pos| contains the offset in |encoder_|
-  // where the length should be encoded. |len| can be used to affect the
-  // encoding, in order to produce invalid lengths. To correctly call this, the
-  // tag must already be encoded immediately prior to |len_pos|, and the
-  // remainder of |encoder_| represents the encoded value.
-  size_t EncodeLength(const Length& len, size_t actual_len, size_t len_pos);
+  // Encodes the length to DER.
+  // |len| can be used to affect the encoding, in order to produce
+  // invalid lengths. |actual_len| is the correct length of the PDU,
+  // and is used when |len| is not. |len_pos| contains the offset in |encoder_|
+  // where the length should be encoded.
+  // To correctly call this, the tag must already be encoded immediately prior
+  // to |len_pos|, and the remainder of |encoder_| represents the encoded value.
+  void EncodeLength(const Length& len, size_t actual_len, size_t len_pos);
 
-  // Writes |raw_len| to |encoder_| at |len_pos| and returns the number of
-  // bytes used to encode |raw_len|.
-  size_t EncodeOverrideLength(const std::string raw_len, const size_t len_pos);
+  // Writes |raw_len| to |encoder_| at |len_pos|.
+  void EncodeOverrideLength(const std::string raw_len, const size_t len_pos);
 
   // Encodes the indefinite-length indicator (X.690 (2015), 8.1.3.6) at
   // |len_pos|, and appends an End-of-Contents (EOC) marker at the end of
-  // |encoder_|, returning number of bytes used to encode indefinite-length
-  // indicator and EOC.
-  size_t EncodeIndefiniteLength(const size_t len_pos);
+  // |encoder_|.
+  void EncodeIndefiniteLength(const size_t len_pos);
 
   // Encodes the length in |actual_len| using the definite-form length (X.690
-  // (2015), 8.1.3-8.1.5 & 10.1) into |encoder_| at |len_pos|, returning number
-  // of bytes used to encode |actual_len|.
-  size_t EncodeDefiniteLength(const size_t actual_len, const size_t len_pos);
+  // (2015), 8.1.3-8.1.5 & 10.1) into |encoder_| at |len_pos|.
+  void EncodeDefiniteLength(const size_t actual_len, const size_t len_pos);
 
-  // Extracts bytes from |val| and inserts them into |enocder_|, returning
-  // number of bytes used to encode |val|.
-  size_t EncodeValue(const Value& val);
+  // Extracts bytes from |val| and inserts them into |enocder_|.
+  void EncodeValue(const Value& val);
 
-  // Converts |value| to a variable-length, big-endian representation and inserts
-  // the result into into |encoder_| at |pos|.
+  // Converts |value| to a variable-length, big-endian representation and
+  // inserts the result into into |encoder_| at |pos|.
   void InsertVariableInt(const size_t value, const size_t pos);
 
   // Returns the number of bytes needed to |base| encode |value| into a
   // variable-length unsigned integer with no leading zeros.
   uint8_t GetVariableIntLen(const uint64_t value, const size_t base);
 
-  // Prints bits in |encoder_|. Used for testing and validation.
-  void PrintEncodedBits();
-  
   size_t depth_;
 };
 
