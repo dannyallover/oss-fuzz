eee871f0172b040f29925c7ff196e02832c9f24e
Danny Halawi <dhalawi@google.com>
2020-07-24 17:52:04 +0000

    pulling latest asn1 pdu

--- projects/certs/asn1_pdu_to_der.cc
+++ projects/certs/asn1_pdu_to_der.cc
@@ -2,92 +2,99 @@
 
 namespace asn1_pdu {
 
-uint8_t ASN1PDUToDER::GetNumBytes(const size_t num, const size_t base) {
+// The maximum level of recursion allowed. Values greater than this will just
+// fail.
+static constexpr size_t kRecursionLimit = 200;
+
+// Signal that the recursion limit has been exceeded by a
+// message or value and to abort further processing.
+static bool kErrRecursionLimitReached = false;
+
+uint8_t ASN1PDUToDER::GetVariableIntLen(const uint64_t value,
+                                          const size_t base) {
   uint8_t base_bits = log2(base);
-  for (uint8_t num_bits = sizeof(num) * 8; num_bits > base_bits;
+  for (uint8_t num_bits = (sizeof(value) - 1) * __CHAR_BIT__; num_bits >= base_bits;
        num_bits -= base_bits) {
-    if ((num >> num_bits)) {
-      return ceil((double)num_bits / base_bits);
+    if (value >> num_bits) {
+      return ceil((double)num_bits / base_bits) + 1;
     }
   }
   // Special-case: zero requires one, not zero bytes.
   return 1;
 }
 
-void ASN1PDUToDER::AppendVariableInt(const size_t value,
-                                          const size_t pos) {
-  std::vector<uint8_t> len_vec;
-  for (uint8_t shift = GetNumBytes(value, 256); shift != 0; --shift) {
-    len_vec.push_back((value >> ((shift - 1) * 8)) & 0xFF);
+void ASN1PDUToDER::InsertVariableInt(const size_t value, const size_t pos) {
+  std::vector<uint8_t> variable_int;
+  for (uint8_t shift = GetVariableIntLen(value, 256); shift != 0; --shift) {
+    variable_int.push_back((value >> ((shift - 1) * __CHAR_BIT__)) & 0xFF);
   }
-  encoder_.insert(encoder_.begin() + pos, len_vec.begin(), len_vec.end());
+  encoder_.insert(encoder_.begin() + pos, variable_int.begin(),
+                  variable_int.end());
 }
 
-size_t ASN1PDUToDER::EncodeOverrideLength(const std::string raw_len,
-                                               const size_t len_pos) {
+void ASN1PDUToDER::EncodeOverrideLength(const std::string& raw_len,
+                                          const size_t len_pos) {
   encoder_.insert(encoder_.begin() + len_pos, raw_len.begin(), raw_len.end());
-  return raw_len.size();
 }
 
-size_t ASN1PDUToDER::EncodeIndefiniteLength(const size_t len_pos) {
-  AppendVariableInt(0x80, len_pos);
+void ASN1PDUToDER::EncodeIndefiniteLength(const size_t len_pos) {
+  encoder_.insert(encoder_.begin() + len_pos, 0x80);
   // The PDU's value is from |len_pos| to the end of |encoder_|, so just add an
   // EOC marker to the end.
-  AppendVariableInt(0x00, encoder_.size());
-  AppendVariableInt(0x00, encoder_.size());
-  return 3;
+  encoder_.push_back(0x00);
+  encoder_.push_back(0x00);
 }
 
-size_t ASN1PDUToDER::EncodeDefiniteLength(const size_t actual_len,
-                                               const size_t len_pos) {
-  AppendVariableInt(actual_len, len_pos);
-  size_t len_num_bytes = GetNumBytes(actual_len, 256);
+void ASN1PDUToDER::EncodeDefiniteLength(const size_t actual_len,
+                                          const size_t len_pos) {
+  InsertVariableInt(actual_len, len_pos);
   // X.690 (2015), 8.1.3.3: The long-form is used when the length is
   // larger than 127.
-  // Note: |len_num_bytes| is not checked here, because it will return
+  // Note: |len_num_bytes| is not checked here, because it will equal
   // 1 for values [128..255], but those require the long-form length.
   if (actual_len > 127) {
     // See X.690 (2015) 8.1.3.5.
     // Long-form length is encoded as a byte with the high-bit set to indicate
     // the long-form, while the remaining bits indicate how many bytes are used
     // to encode the length.
-    AppendVariableInt((0x80 | len_num_bytes), len_pos);
-    len_num_bytes += 1;
+    size_t len_num_bytes = GetVariableIntLen(actual_len, 256);
+    encoder_.insert(encoder_.begin() + len_pos, (0x80 | len_num_bytes));
   }
-  return len_num_bytes;
 }
 
-size_t ASN1PDUToDER::EncodeLength(const Length& len,
-                                       const size_t actual_len,
-                                       const size_t len_pos) {
+void ASN1PDUToDER::EncodeLength(const Length& len,
+                                  const size_t actual_len,
+                                  const size_t len_pos) {
   if (len.has_length_override()) {
-    return EncodeOverrideLength(len.length_override(), len_pos);
+    EncodeOverrideLength(len.length_override(), len_pos);
   } else if (len.has_indefinite_form() && len.indefinite_form()) {
-    return EncodeIndefiniteLength(len_pos);
+    EncodeIndefiniteLength(len_pos);
   } else {
-    return EncodeDefiniteLength(actual_len, len_pos);
+    EncodeDefiniteLength(actual_len, len_pos);
   }
 }
 
-size_t ASN1PDUToDER::EncodeValue(const Value& val) {
-  size_t len = 0;
+void ASN1PDUToDER::EncodeValue(const Value& val) {
   for (const auto& val_ele : val.val_array()) {
+    if (kErrRecursionLimitReached) {
+      // If the message exceeds the recursion limit, abort processing the
+      // protobuf in order to limit uninteresting work.
+      return;
+    }
     if (val_ele.has_pdu()) {
-      len += EncodePDU(val_ele.pdu());
+      EncodePDU(val_ele.pdu());
     } else {
-      len += val_ele.val_bits().size();
       encoder_.insert(encoder_.end(), val_ele.val_bits().begin(),
                       val_ele.val_bits().end());
     }
   }
-  return len;
 }
 
-uint8_t ASN1PDUToDER::EncodeHighTagNumberForm(const uint8_t id_class,
-                                                   const uint8_t encoding,
-                                                   const uint32_t tag_num) {
+void ASN1PDUToDER::EncodeHighTagNumberForm(const uint8_t id_class,
+                                             const uint8_t encoding,
+                                             const uint32_t tag_num) {
   // The high-tag-number form base 128 encodes |tag_num| (X.690 (2015), 8.1.2).
-  uint8_t num_bytes = GetNumBytes(tag_num, 128);
+  uint8_t num_bytes = GetVariableIntLen(tag_num, 128);
   // High-tag-number form requires the lower 5 bits of the identifier to be set
   // to 1 (X.690 (2015), 8.1.2.4.1).
   uint64_t id_parsed = (id_class | encoding | 0x1F);
@@ -99,11 +106,10 @@ uint8_t ASN1PDUToDER::EncodeHighTagNumberForm(const uint8_t id_class,
     id_parsed <<= 8;
   }
   id_parsed |= (tag_num & 0x7F);
-  AppendVariableInt(id_parsed, encoder_.size());
-  return num_bytes + 1;
+  InsertVariableInt(id_parsed, encoder_.size());
 }
 
-uint8_t ASN1PDUToDER::EncodeIdentifier(const Identifier& id) {
+void ASN1PDUToDER::EncodeIdentifier(const Identifier& id) {
   // The class comprises the 7th and 8th bit of the identifier (X.690
   // (2015), 8.1.2).
   uint8_t id_class = static_cast<uint8_t>(id.id_class()) << 6;
@@ -116,41 +122,37 @@ uint8_t ASN1PDUToDER::EncodeIdentifier(const Identifier& id) {
   // When the tag number is greater than or equal to 31, encode with a single
   // byte; otherwise, use the high-tag-number form (X.690 (2015), 8.1.2).
   if (tag_num >= 31) {
-    return EncodeHighTagNumberForm(id_class, encoding, tag_num);
+    EncodeHighTagNumberForm(id_class, encoding, tag_num);
+  } else {
+    encoder_.push_back(static_cast<uint8_t>(id_class | encoding | tag_num));
   }
-  AppendVariableInt((id_class | encoding | tag_num), encoder_.size());
-  return 1;  // low-tag-number form requires 1 byte to encode.
 }
 
-size_t ASN1PDUToDER::EncodePDU(const PDU& pdu) {
-  ++depth_;
+void ASN1PDUToDER::EncodePDU(const PDU& pdu) {
   // Artifically limit the stack depth to avoid stack overflow.
-  if (depth_ > 67000) {
-    return 0;
+  if (depth_ > kRecursionLimit) {
+    kErrRecursionLimitReached = true;
+    return;
+  } else {
+    ++depth_;
   }
-  uint8_t id_len = EncodeIdentifier(pdu.id());
+  EncodeIdentifier(pdu.id());
   size_t len_pos = encoder_.size();
-  size_t val_len = EncodeValue(pdu.val());
-  size_t len_len = EncodeLength(pdu.len(), val_len, len_pos);
+  EncodeValue(pdu.val());
+  EncodeLength(pdu.len(), encoder_.size() - len_pos, len_pos);
   --depth_;
-  return id_len + val_len + len_len;
-}
-
-void ASN1PDUToDER::PrintEncodedBits() {
-  for (const uint8_t byte : encoder_) {
-    for (int i = 7; i >= 0; --i) {
-      if (((byte >> i) & 0x01)) {
-        std::cout << "1";
-      } else {
-        std::cout << "0";
-      }
-    }
-    std::cout << " ";
-  }
 }
 
 std::vector<uint8_t> ASN1PDUToDER::PDUToDER(const PDU& pdu) {
+  // Reset the previous state.
+  encoder_.clear();
+  depth_ = 0;
+  kErrRecursionLimitReached = false;
+
   EncodePDU(pdu);
+  if (kErrRecursionLimitReached) {
+    encoder_.clear();
+  }
   return encoder_;
 }
 
